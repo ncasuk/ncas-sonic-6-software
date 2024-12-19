1c120f4eef21f062c4d965a46c4bcf4c336f70ce
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2024-12-19 13:31:42 +0000

    Make output directory if needed

--- scripts/make_netcdf.sh
+++ scripts/make_netcdf.sh
@@ -13,4 +13,6 @@ if [ ! -f /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_lon
     exit 1
 fi
 
+
+mkdir -p /gws/pw/j07/ncas_obs_vol1/cvao/processing/20241127_longterm/${DATE:0:4}/${DATE:4:2}
 python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
