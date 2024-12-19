fe3cf9cf7a4f9924203172fde056056ea17a7644
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2024-12-19 13:19:29 +0000

    Correct file path for raw_data

--- scripts/make_netcdf.sh
+++ scripts/make_netcdf.sh
@@ -9,9 +9,9 @@ python_env=${DIR}/../venv/bin/activate
 source $python_env
 
 # check file exists
-if [ ! -f /gws/pw/j07/ncas_obs_vol1/cvao/raw-data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv ]; then
+if [ ! -f /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv ]; then
     echo "File not found!"
     exit 1
 fi
 
-python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw-data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
+python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
