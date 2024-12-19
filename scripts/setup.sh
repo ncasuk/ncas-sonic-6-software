ccab52b12475b658a767a694b0eb2158ac9a09e1
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2024-12-19 13:12:05 +0000

    Exit if making environment fails

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,11 @@ DIR=$(dirname "$(readlink -f "$0")")
 
 # create python venv and install requirements
 python3 -m venv ${DIR}/../venv
+# if previous command fails, exit
+if [ $? -ne 0 ]; then
+    echo "Failed to create python venv"
+    exit 1
+fi
 source ${DIR}/../venv/bin/activate
 pip install -r ${DIR}/../proc_netcdf/requirements.txt
 
