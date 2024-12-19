e4a4d9258c84f016bf190f17ff818daceeae07a6
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2024-12-19 14:20:41 +0000

    Directly call python environment, rather than activate

--- scripts/make_netcdf.sh
+++ scripts/make_netcdf.sh
@@ -4,9 +4,6 @@ DATE=$1
 
 DIR=$(dirname "$(readlink -f "$0")")
 
-# activate mamba environment
-conda activate sonic6
-
 # check file exists
 if [ ! -f /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv ]; then
     echo "File not found!"
@@ -15,4 +12,4 @@ fi
 
 
 mkdir -p /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2}
-python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
+/home/users/earjham/miniforge3/envs/sonic6/bin/python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
