0b1aff440e1fd3327a79b87caf81209f5288a8e6
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2025-02-05 10:43:30 +0000

    Add consistency check for wind direction and change version number

--- proc_netcdf/process_sonic.py
+++ proc_netcdf/process_sonic.py
@@ -21,7 +21,61 @@ import ncas_amof_netcdf_template as nant
 import datetime as dt
 
 
-def main(infile, outdir="./", metadata_file="metadata.json"):
+def find_closest_time_match(df_small: pl.DataFrame, df_large: pl.DataFrame) -> pl.DataFrame:
+    if "time" not in df_small.columns or not all(isinstance(x, dt.datetime) for x in df_small["time"]):
+        raise ValueError("df_small must have a 'time' column of datetime objects.")
+    if "time" not in df_large.columns or not all(isinstance(x, dt.datetime) for x in df_large["time"]):
+        raise ValueError("df_large must have a 'time' column of datetime objects.")
+    if df_small.is_empty():
+        return pl.DataFrame()  # Return empty DataFrame if df_small is empty
+    df_combined = []  # List to hold the combined rows
+    for row in df_small.iter_rows(named=True):  # Iterate through rows of df_small
+        target_time = row["time"]
+        closest_match = None
+        min_diff = dt.timedelta.max
+        for large_row in df_large.iter_rows(named=True): # Iterate through rows of df_large
+            diff = abs(target_time - large_row["time"])
+            if diff < min_diff:
+                min_diff = diff
+                closest_match = large_row
+            else:
+                break
+        if closest_match:
+            combined_row = {**row, **{k: v for k, v in closest_match.items() if k != "time"}}
+            df_combined.append(combined_row)
+    return pl.DataFrame(df_combined)
+
+
+def check_wind_dir_consistency(df, aws_7_file, diff=45):
+    # read data and convert times to datetime
+    if aws_7_file is not None:
+        aws_7 = pl.read_csv(aws_7_file, columns=["Timestamp (UTC)", "Winddir / °"], new_columns=["time", "winddir"])
+        aws_7 = aws_7.with_columns(pl.col("time").str.to_datetime("%Y-%m-%d %H:%M:%S", time_zone="UTC"))
+        print("Read AWS-7 data")
+    else:
+        raise ValueError("No data files provided for consistency check")
+
+    # check wind direction consistency
+    print("Finding closest time match")
+    df = find_closest_time_match(df, aws_7)
+    print("found")
+    new_status = []
+    for i in range(len(df)):
+        if df["status"][i] in ["00", 0] and df["winddir"][i] != "NULL":
+            if 360-diff > abs(df["wind_from_direction"][i] - float(df["winddir"][i])) > diff:
+                new_status.append(8)
+            else:
+                new_status.append(df["status"][i])
+        else:
+            new_status.append(df["status"][i])
+    df = df.with_columns([
+        pl.Series(new_status).alias("status"),
+    ])
+
+    return df
+
+
+def main(infile, outdir="./", metadata_file="metadata.json", aws_7_file=None):
     # read data
     df = pl.read_csv(infile, has_header=False, new_columns=["time", "node", "gill_u", "gill_v", "units", "status", "check"])
     
@@ -66,7 +120,7 @@ def main(infile, outdir="./", metadata_file="metadata.json"):
     unix_times, day_of_year, years, months, days, hours, minutes, seconds, time_coverage_start_unix, time_coverage_end_unix, file_date = nant.util.get_times(df["time"])
 
     # Create netCDF file
-    nc = nant.create_netcdf.main("ncas-sonic-6", date=file_date, dimension_lengths={"time": len(unix_times)}, products="mean-winds", file_location=outdir)
+    nc = nant.create_netcdf.main("ncas-sonic-6", date=file_date, dimension_lengths={"time": len(unix_times)}, products="mean-winds", file_location=outdir, product_version="1.1")
     if isinstance(nc, list):
         print("[WARNING] Unexpectedly got multiple netCDFs returned from nant.create_netcdf.main, just using first file...")
         nc = nc[0]
@@ -88,13 +142,25 @@ def main(infile, outdir="./", metadata_file="metadata.json"):
     # Add computed wind data to netCDF file
     wind_speed = pl.Series((df["met_u"] ** 2 + df["met_v"] ** 2) ** 0.5).round(2)
     nant.util.update_variable(nc, "wind_speed", wind_speed)
+    # add wind_speed to df
+    df = df.with_columns([
+        wind_speed.alias("wind_speed"),
+    ])
     
-    wind_from_direction = (270 - np.arctan2(df["met_v"], df["met_u"]) * 180 / np.pi) % 360
+    wind_from_direction = pl.Series((270 - np.arctan2(df["met_v"], df["met_u"]) * 180 / np.pi) % 360)
     nant.util.update_variable(nc, "wind_from_direction", wind_from_direction)
+    # add wind_direction to df
+    df = df.with_columns([
+        wind_from_direction.alias("wind_from_direction"),
+    ])
+
+    # check wind direction consistency with other aws data
+    if aws_7_file is not None:
+        df = check_wind_dir_consistency(df, aws_7_file)
     
     # Check status codes to create qc flag data
     qc_vals = []
-    qc_meanings = ["not_used", "good_data", "insufficient_samples_in_average_period_along_u_axis", "insufficient_samples_in_average_period_along_v_axis", "insufficient_samples_in_average_period_along_both_axis"]
+    qc_meanings = ["not_used", "good_data", "insufficient_samples_in_average_period_along_u_axis", "insufficient_samples_in_average_period_along_v_axis", "insufficient_samples_in_average_period_along_both_axis", "wind_direction_inconsistent_with_other_weather_stations"]
     
     for s in df["status"]:
         if s in ["00", 0]:
@@ -109,6 +175,9 @@ def main(infile, outdir="./", metadata_file="metadata.json"):
         elif s in ["04", 4]:
             # both axis
             qc_vals.append(4)
+        elif s in ["08", 8]:
+            # inconsistent with other weather stations
+            qc_vals.append(5)
         else:
             print(s)
             raise ValueError("Unknown status code")
@@ -140,11 +209,18 @@ def main(infile, outdir="./", metadata_file="metadata.json"):
     nant.remove_empty_variables.main(file_name)
 
 
+def none_or_str(value):
+    if value == 'None':
+        return None
+    return value
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Process Gill Sonic data to netCDF")
     parser.add_argument("infile", type=str, help="Input file")
     parser.add_argument("-o", "--outdir", type=str, default="./", help="Output directory")
     parser.add_argument("-m", "--metadata_file", type=str, default="metadata.json", help="Metadata file")
+    parser.add_argument("--aws_7_file", type=none_or_str, default=None, help="Data file from ncas-aws-7 for consistency check")
     args = parser.parse_args()
-    main(args.infile, outdir=args.outdir, metadata_file=args.metadata_file)
+    main(args.infile, outdir=args.outdir, metadata_file=args.metadata_file, aws_7_file=args.aws_7_file)
--- scripts/make_netcdf.sh
+++ scripts/make_netcdf.sh
@@ -4,12 +4,17 @@ DATE=$1
 
 DIR=$(dirname "$(readlink -f "$0")")
 
-# check file exists
+# check sonic file exists
 if [ ! -f /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv ]; then
     echo "File not found!"
     exit 1
 fi
 
-
 mkdir -p /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2}
-/home/users/earjham/miniforge3/envs/sonic6/bin/python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
+
+# if aws 7 file exists, pass it to python, if not then don't
+if [ -f /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-aws-7/data/CUSTOM-ARC-${DATE:0:4}-${DATE:4:2}-${DATE:6:2}-METRIC.csv ]; then
+    /home/users/earjham/miniforge3/envs/sonic6/bin/python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json --aws_7_file /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-aws-7/data/CUSTOM-ARC-${DATE:0:4}-${DATE:4:2}-${DATE:6:2}-METRIC.csv
+else
+    /home/users/earjham/miniforge3/envs/sonic6/bin/python ${DIR}/../proc_netcdf/process_sonic.py /gws/pw/j07/ncas_obs_vol1/cvao/raw_data/ncas-sonic-6/data/20241127_longterm/${DATE:0:4}/${DATE:4:2}/${DATE}_ncas-sonic-6.csv -o /gws/pw/j07/ncas_obs_vol1/cvao/processing/ncas-sonic-6/20241127_longterm/${DATE:0:4}/${DATE:4:2} -m ${DIR}/../proc_netcdf/metadata.json
+fi
